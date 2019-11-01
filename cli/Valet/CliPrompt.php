7a01758a909c6628a3d10886e10432001766f46e
Divyank <dvmunjapara@gmail.com>
2019-11-01 19:41:51 +0530

    * Added code to receive CLI input from user

---
+++ cli/Valet/CliPrompt.php
@@ -0,0 +1,38 @@
+<?php
+
+
+namespace Valet;
+
+
+class CliPrompt
+{
+
+    /**
+     * Prompts the user for input and shows what they type
+     *
+     * @param $question
+     * @param bool $hidden
+     * @param null $suggestion
+     * @param null $default
+     * @return string
+     */
+    public function prompt($question,$hidden = false,$suggestion = null,$default = null)
+    {
+        $question = $suggestion? "$question [$suggestion]":$question;
+        $question = $default? "$question ($default)":$question;
+        $question .= PHP_EOL;
+
+        print_r($question);
+
+        if($hidden) system('stty -echo');
+        $answer = self::trimAnswer(fgets(STDIN, 4096));
+        if($hidden) system('stty echo');
+
+        return !empty($answer)?$answer:$default;
+    }
+
+    private static function trimAnswer($str)
+    {
+        return preg_replace('{\r?\n$}D', '', $str);
+    }
+}
